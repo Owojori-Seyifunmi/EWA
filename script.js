9c37953e1926f41f7d3c602984c5a852aed5cc0e
Owojori Oluwaseyifunmi Paul <spowojori@gmail.com>
2025-08-09 11:56:05 +0100

    Updated project files

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navToggle = document.querySelector('.nav-toggle');
     const links = document.querySelector('.links');
-    
+    const link = document.querySelectorAll('.link');
+
     navToggle.addEventListener('click', function() {
         links.classList.toggle('show-links');
+        
+    });
+
+    link.forEach(item => {
+        item.addEventListener('click', function() {
+            links.classList.remove('show-links');
+        });
     });
 });
